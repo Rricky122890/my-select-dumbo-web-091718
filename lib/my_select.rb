b2f2e8cb4e896965ad21ef2d6e8d8d1e18288fd7
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-08 20:51:37 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,13 @@
 def my_select(collection)
- 
+ # your code here!
   return "This block should not run!" if collection.empty?
- 
- collection.select |item| { 
-   yield(item) 
- }
- else
- 
+   new_arr = []
+  i = 0
+  while i < collection.length
+    if yield(collection[i])
+      new_arr << collection[i]
+    end
+    i += 1
+  end
+   return new_arr
 end
